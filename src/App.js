e50ebc597be268dd6ef01c291819c2bd35b49b6b
obedpollo <obed_cs@hotmail.com>
2023-01-19 16:11:49 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+//import logo from './images/FreeCodeCamp_logo.png';
+import logo from './images/logo.png';
+import Boton from './components/Boton'
+import Contador from './components/Contador';
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src = { logo }
+          alt = 'Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+          numClics = {numClics}
+        />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
